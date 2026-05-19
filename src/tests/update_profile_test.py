c135de6aa8a4d7fa1110c543b1781a0d0b2fdc83
nik54 <nik54a@gmail.com>
2026-05-19 22:06:41 +0300

    -

--- src/tests/update_profile_test.py
+++ src/tests/update_profile_test.py
@@ -6,10 +6,10 @@ from src.main.api.models.create_user_request import CreateUserRequest
 
 @pytest.mark.api
 class TestUpdateProfile:
-    def test_update_name_in_the_profile(self, api_manager: ApiManager, created_user_request: CreateUserRequest):
-        profile = api_manager.customer_management_steps.update_profile(created_user_request, name="John Smith")
+    def test_update_name_in_the_profile(self, api_manager: ApiManager, user_request: CreateUserRequest):
+        profile = api_manager.customer_management_steps.update_profile(user_request, name="John Smith")
         assert profile.customer.name == "John Smith"
-        assert api_manager.customer_management_steps.get_profile(created_user_request).name == "John Smith"
+        assert api_manager.customer_management_steps.get_profile(user_request).name == "John Smith"
 
     @pytest.mark.parametrize(
         argnames='name',
@@ -21,6 +21,6 @@ class TestUpdateProfile:
             "1John Smith",
         ]
     )
-    def test_update_profile_bad_name(self, api_manager: ApiManager, created_user_request: CreateUserRequest, name: str):
-        api_manager.customer_management_steps.update_profile_bad_name(created_user_request, name=name, error_text="Name must contain two words with letters only")
-        assert api_manager.customer_management_steps.get_profile(created_user_request).name is None
+    def test_update_profile_bad_name(self, api_manager: ApiManager, user_request: CreateUserRequest, name: str):
+        api_manager.customer_management_steps.update_profile_bad_name(user_request, name=name, error_text="Name must contain two words with letters only")
+        assert api_manager.customer_management_steps.get_profile(user_request).name is None
