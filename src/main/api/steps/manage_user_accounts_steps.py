1f2ed1c0b3278eb347ad1619d40356a8320954b6
nik54 <nik54a@gmail.com>
2026-05-22 16:17:26 +0300

    вынес проверки в ModelAssertions

--- src/main/api/steps/manage_user_accounts_steps.py
+++ src/main/api/steps/manage_user_accounts_steps.py
@@ -1,5 +1,5 @@
+from src.main.api.models.comparison.model_assertions import ModelAssertions
 from src.main.api.models.create_user_request import CreateUserRequest
-from src.main.api.models.get_profile_response import GetProfileResponse
 from src.main.api.models.increase_deposit_request import IncreaseDepositRequest
 from src.main.api.models.increase_deposit_response import IncreaseDepositResponse, TransactionResponse
 from src.main.api.models.transfer_request import TransferRequest
@@ -25,7 +25,7 @@ class ManageUserAccountsSteps(BaseSteps):
             ResponseSpecs.request_returns_ok()
         ).post(increase_deposit_request)
 
-        assert increase_deposit_response.id == id
+        ModelAssertions(increase_deposit_request, increase_deposit_response).match()
         assert increase_deposit_response.transactions
 
         return increase_deposit_response
@@ -55,10 +55,8 @@ class ManageUserAccountsSteps(BaseSteps):
             ResponseSpecs.request_returns_ok()
         ).post(transfer_request)
 
-        assert transfer_response.receiverAccountId == receiver_account_id
-        assert transfer_response.amount == amount
+        ModelAssertions(transfer_request, transfer_response).match()
         assert transfer_response.message == "Transfer successful"
-        assert transfer_response.senderAccountId == sender_account_id
 
         return transfer_response
 
