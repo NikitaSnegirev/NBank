5ab36a9957fbfbad0921058d4662ba026da7ca18
nik54 <nik54a@gmail.com>
2026-06-01 20:03:13 +0300

    генерация имени профиля

--- src/tests/update_profile_test.py
+++ src/tests/update_profile_test.py
@@ -2,15 +2,20 @@ import pytest
 
 from src.main.api.classes.api_manager import ApiManager
 from src.main.api.constans.error_messages import NAME_ERROR
+from src.main.api.generators.random_model_generator import RandomModelGenerator
 from src.main.api.models.create_user_request import CreateUserRequest
+from src.main.api.models.update_profile_request import UpdateProfileRequest
 
 
 @pytest.mark.api
 class TestUpdateProfile:
     def test_update_name_in_the_profile(self, api_manager: ApiManager, user_request: CreateUserRequest):
-        profile = api_manager.customer_management_steps.update_profile(user_request, name="John Smith")
-        assert profile.customer.name == "John Smith"
-        assert api_manager.customer_management_steps.get_profile(user_request).name == "John Smith"
+        update_profile_request = RandomModelGenerator.generate(UpdateProfileRequest)
+
+        profile = api_manager.customer_management_steps.update_profile(user_request, name=update_profile_request.name)
+
+        assert profile.customer.name == update_profile_request.name
+        assert api_manager.customer_management_steps.get_profile(user_request).name == update_profile_request.name
 
     @pytest.mark.parametrize(
         argnames='name',
